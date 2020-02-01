7f502ef1e9a89bdc864ccb15eb7ac9e0a6347eb0
Paul Rock <paul@drteam.rocks>
2020-02-01 14:51:43 +0300

    middleware check added to routes, names of used endpoints updated

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,12 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', 'Admin\IndexController@index')->name('home');
 });
-
-Route::view('/test-apollo', 'test-apollo');
