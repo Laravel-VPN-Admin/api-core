b3499607a8a9b47fdd0ea3bd47a76e25143d7ef3
Paul Rock <paul@drteam.rocks>
2020-02-08 22:34:38 +0300

    pages for detalization about single elements from database

---
+++ resources/js/components/Pages/Group.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mb-5">
+    group
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      id: Number
+    }
+  }
+</script>
---
+++ resources/js/components/Pages/User.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mb-5">
+    user
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      id: Number
+    }
+  }
+</script>
