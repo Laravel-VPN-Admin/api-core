526d84a7670cab1e047cf4e8bdf066457d17ac17
Paul Rock <paul@drteam.rocks>
2020-02-08 16:23:12 +0300

    refactoring of views, frontend moved to single file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,32 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <title>{{ config('app.name', 'Laravel VPN Admin') }}</title>
 
-@section('content')
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <!-- Styles -->
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+  <!-- Styles stacked -->
+  @stack('header:styles')
+
+</head>
+<body>
+
+<main id="app">
   <app />
-@endsection
+</main>
+
+<!-- Scripts -->
+<script src="{{ mix('js/manifest.js') }}"></script>
+<script src="{{ mix('js/vendor.js') }}"></script>
+<script src="{{ mix('js/app.js') }}"></script>
+
+<!-- Scripts stacked -->
+@stack('footer:scripts')
+
+</body>
+</html>
