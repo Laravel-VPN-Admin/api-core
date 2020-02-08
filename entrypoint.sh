1b65bf37f8d6a37ed385b104978045a742006c6e
Paul Rock <paul@drteam.rocks>
2020-02-08 21:20:45 +0300

    bugs for creating container is fixed

---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+role=${CONTAINER_ROLE:-app}
+env=${APP_ENV:-production}
+
+cd /app
+
+if [ "$role" = "app" ]; then
+
+    echo "Caching configuration..."
+    composer dump-autoload
+    php artisan optimize:clear
+
+    echo "Migrate and generate API doc..."
+    php artisan migrate --force
+    /entrypoint.sh
+
+elif [ "$role" = "queue" ]; then
+
+    echo "Queue role"
+    php artisan queue:work --sleep=10 --tries=3 --timeout=10000
+
+elif [ "$role" = "scheduler" ]; then
+
+    while [ true ]
+    do
+      php artisan schedule:run --verbose --no-interaction &
+      sleep 60
+    done
+
+else
+    echo "Could not match the container role \"$role\""
+    exit 1
+fi
