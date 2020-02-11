612cc167dccbe543cb84de290684270768910a5e
Braxify <n.artur2281337@gmail.com>
2020-02-11 12:16:27 +0200

    Edit Login Component

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -12,26 +12,32 @@
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                   </div>
                   <div class="alert alert-danger" v-if="error" role="alert">
-   error isn't empty!
+                    error isn't empty!
                   </div>
                   <div class="user">
                     <div class="form-group">
-                      <input type="email" v-model="form.email" class="form-control form-control-user" id="exampleInputEmail"
-                             aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <input
+                        type="email"
+                        v-model="form.email"
+                        class="form-control form-control-user"
+                        id="exampleInputEmail"
+                        aria-describedby="emailHelp"
+                        placeholder="Enter Email Address..."
+                      />
                     </div>
                     <div class="form-group">
-                      <input type="password" v-model="form.password" class="form-control form-control-user" id="exampleInputPassword"
-                             placeholder="Password">
+                      <input
+                        type="password"
+                        v-model="form.password"
+                        class="form-control form-control-user"
+                        id="exampleInputPassword"
+                        placeholder="Password"
+                      />
                     </div>
 
-                    <button
-                      class="btn btn-primary btn-user btn-block"
-                      @click="login()"
-                      :disabled="!isSubmitEnabled()"
-                    >
+                    <button class="btn btn-primary btn-user btn-block" @click="login()" :disabled="!isSubmitEnabled()">
                       Login
                     </button>
-
                   </div>
                 </div>
               </div>
@@ -40,50 +46,48 @@
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-  export default {
-
-    data() {
-      return {
-        form:  {
-          email:    null,
-          password: null
-        },
-        error: null
-      }
-    },
-
-    methods: {
-      /**
-       * Check if submit button can be clicked
-       * @returns {boolean}
-       */
-      isSubmitEnabled() {
-        return this.form.email && this.form.password;
+export default {
+  data() {
+    return {
+      form: {
+        email: null,
+        password: null
       },
-    
-      /**
-       * Initiate login logic based on API token
-       */
-      login() {
-        this.$store.dispatch('login', this.form)
-          .then(response => {
-            if (!!this.$store.state.token) {
-              Vue.$cookies.set('token', this.$store.state.token);
-              this.error = null;
-              this.$router.push({name: 'dashboard'});
-            }
-          })
-          .catch(error => {
-            console.error(error);
-            this.error = "Username or password is incorrect"
-          })
-      }
+      error: null
+    };
+  },
+
+  methods: {
+    /**
+     * Check if submit button can be clicked
+     * @returns {boolean}
+     */
+    isSubmitEnabled() {
+      return this.form.email && this.form.password;
     },
 
+    /**
+     * Initiate login logic based on API token
+     */
+    login() {
+      this.$store
+        .dispatch("login", this.form)
+        .then(response => {
+          if (!!this.$store.state.token) {
+            Vue.$cookies.set("token", this.$store.state.token);
+            this.error = null;
+            this.$router.push({ name: "dashboard" });
+          }
+        })
+        .catch(error => {
+          console.error(error);
+          this.error = "Username or password is incorrect";
+        });
+    }
   }
+};
 </script>
