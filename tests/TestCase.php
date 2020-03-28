524a84c4977a85847ac1abce61a51200b7956294
Paul Rock <paul@drteam.rocks>
2020-03-28 16:16:54 +0300

    tests moved to sqlite logic

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,11 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, DatabaseTransactions, MakesGraphQLRequests;
+    use CreatesApplication, MakesGraphQLRequests, RefreshDatabase;
 }
