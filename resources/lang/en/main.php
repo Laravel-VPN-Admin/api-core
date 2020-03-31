1db2c6b6affd043beb23defb03b86d23ef1aa0ed
Paul Rock <paul@drteam.rocks>
2020-03-31 22:30:07 +0300

    basic translations added

---
+++ resources/lang/en/main.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'links'  => [
+        'dashboard' => 'Dashboard',
+        'servers'   => 'Servers',
+        'groups'    => 'Groups',
+        'users'     => 'Users',
+        'logs'      => 'Logs',
+        'settings'  => 'Settings',
+    ],
+    'titles' => [
+        'dashboard' => 'Dashboard',
+        'servers'   => 'List of servers',
+        'groups'    => 'Groups of servers and users',
+        'users'     => 'List of users',
+        'logs'      => 'Logs from servers',
+        'settings'  => 'Settings of system',
+    ],
+];
