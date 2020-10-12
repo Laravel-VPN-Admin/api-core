f27c7a84011679560bee07d23e76fcda1e0e31c8
Paul Rock <paul@drteam.rocks>
2020-10-12 20:21:04 +0300

    LogCreate tests added

---
+++ tests/Feature/GraphQL/Mutations/LogsTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Feature\GraphQL\Mutations;
+
+use Tests\TestCase;
+
+class LogsTest extends TestCase
+{
+    /**
+     * @var \App\Models\Server
+     */
+    private $server;
+
+    /**
+     * @var \App\User
+     */
+    private $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->server = factory(\App\Models\Server::class)->create([
+            'hostname' => 'Isengard',
+            'ipv4'     => '10.2.3.4',
+            'ipv6'     => 'i:dd:qd',
+        ]);
+
+        $this->user = factory(\App\User::class)->create([
+            'name'     => 'Frodo Baggins',
+            'email'    => 'frodo@bag.end',
+            'password' => \Hash::make('MyPrecious1')
+        ]);
+
+        $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->user->api_token,
+            'Accept'        => 'application/json',
+        ]);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testLogCreate()
+    {
+        $response = $this->graphQL(/** @lang GraphQL */ '
+             mutation {
+              createLog (
+                input: {
+                  code:100
+                  message:"Test message for pusher"
+                  user_id:' . $this->user->id . '
+                  server_id:' . $this->server->id . '
+                }
+              )
+              {
+                id
+                message
+                user {
+                  id
+                  name
+                }
+                server {
+                  id
+                  hostname
+                }
+              }
+            }
+        ');
+
+        $log = $response->json('data.createLog');
+
+        $this->assertEquals('Test message for pusher', $log['message']);
+        $this->assertDatabaseHas('logs', ['id' => $log['id']]);
+    }
+}
